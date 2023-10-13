be7baef2167697f79e62a68972f6f412497c5b83
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2023-10-13 13:30:16 +0300

    Добавлен вывод жены и мужа в дереве

---
+++ hw1/Family.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class FamilyMember
+    {
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Father { get; set; }
+        public string Name { get; set; }
+        public Gender Sex { get; set; }
+
+        public void Info(int indent = 0, string info = "")
+        {
+            Console.WriteLine($"{new String('-', indent)}Имя {this.Name} ({info})");
+        }
+        public virtual void Print(int indent = 0, string info = "")
+        {
+            Info(indent, info);
+        }
+
+
+    }
+
+    public class AdultFamilyMember : FamilyMember
+    {
+        public FamilyMember[] Children { get; set; }
+        public override void Print(int indent = 0, string info = "")
+        {
+            base.Print(indent);
+            if (Children != null && Children.Length != 0)
+            {
+                bool searchWifeOrHusband = true;
+                foreach (var child in Children)
+                {
+                    if (searchWifeOrHusband)
+                    {
+                        if (child.Father != null && child.Father.Equals(this) && child.Mother != null) 
+                        {
+                            (child.Mother as AdultFamilyMember)?.PrintWifeOrHusband(indent);
+                        }
+                        else if(child.Mother != null && child.Mother.Equals(this) && child.Father != null)
+                        {
+                            (child.Father as AdultFamilyMember)?.PrintWifeOrHusband(indent);
+                        }
+                        searchWifeOrHusband = false;
+                    }
+                    child.Print(indent * 2);
+                }
+            }
+        }
+        void PrintWifeOrHusband(int indent)
+        {
+            string info;
+            if (this.Sex == Gender.Female)
+                info = "жена";
+            else
+                info = "муж";
+            base.Print(indent, info);
+        }
+    }
+}
--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using hw1;
+
+var grand_father = new AdultFamilyMember() { Mother = null, Father = null, Name = "Bob", Sex = Gender.Male };
+var father = new AdultFamilyMember() { Mother = null, Father = grand_father, Name = "Jack", Sex = Gender.Male };
+var mother = new AdultFamilyMember() { Mother = null, Father = null, Name = "Ann", Sex = Gender.Female};
+var son1 = new FamilyMember() { Mother = mother, Father = father, Name = "Bill", Sex = Gender.Male };
+var son2 = new FamilyMember() { Mother = mother, Father = father, Name = "Tom",  Sex = Gender.Male };
+var son3 = new FamilyMember() { Mother = mother, Father = father, Name = "Alan", Sex = Gender.Male };
+grand_father.Children = new FamilyMember[] { father };
+father.Children = new FamilyMember[] {son1, son2, son3 };
+mother.Children = new FamilyMember[] {son1, son2, son3 };
+grand_father.Print(2);
+Console.ReadLine();
