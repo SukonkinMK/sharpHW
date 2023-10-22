ea1604f49811c2a69cae5275868c9e9d5ba6d7d5
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2023-10-22 12:20:35 +0300

    Переопределены операторы неявного приведения из byte, short, int и long в класс Bits

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using hw1;
+
+byte testByte = 1;
+Bits bit1 = testByte;
+short testShort = 2;
+Bits bit2 = testShort;
+int testInt = 3;
+Bits bit3 = testInt;
+long testLong = 4;
+Bits bit4 = testLong;
+Console.ReadLine();
