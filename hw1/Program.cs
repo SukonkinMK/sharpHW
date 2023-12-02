dd5728071f5550821734900055f9fd4d91f89665
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2023-12-02 10:24:19 +0300

    Выполнено дз

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using hw1;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var movieJson = "{\"Name\":\"Squid Game\",\"Genre\":\"Thriller\",\"Rating\":8.1,\"Budget\":20000000}";
+        var movie = JsonSerializer.Deserialize<Movie>(movieJson);
+        var xmlSerializer = new XmlSerializer(typeof(Movie));
+        var sb = new StringBuilder();
+        using (var xmlWriter = XmlWriter.Create(sb))
+        {
+            xmlSerializer.Serialize(xmlWriter, movie);
+        }
+        Console.WriteLine(sb.ToString());
+        Console.ReadLine();
+    }
+}
