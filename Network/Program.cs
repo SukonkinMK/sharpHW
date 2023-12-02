39d553de27135c6c561f639cc0549b2e59b1cce6
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2023-12-02 14:00:21 +0300

    Рефакторинг

--- Network/Program.cs
+++ Network/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
