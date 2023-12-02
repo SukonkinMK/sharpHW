ff728874001fa1b7ae3bbe732d7969dcc3b4d235
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2023-12-02 14:20:07 +0300

    Выполнено задание

--- Network/Program.cs
+++ Network/Program.cs
@@ -1,11 +1,43 @@
-﻿using System.Net.Sockets;
+﻿using Network;
+using System.Net.Sockets;
 using System.Net;
 using System.Text;
 
-public class Program
+internal class Program
 {
     static void Main(string[] args)
     {
-
+        Server("Hello");
     }
+
+    public void task1()
+    {
+        Message msg = new Message() { Text = "Hello", DateTime = DateTime.Now, NicknameFrom = "Artem", NicknameTo = "All" };
+        string json = msg.SerializeMessageToJson();
+        Console.WriteLine(json);
+        Message? msgDeserialized = Message.DeserializeFromJson(json);
+    }
+
+    public static void Server(string name)
+    {
+        UdpClient udpClient = new UdpClient(12345);
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        Console.WriteLine("Сервер ждет сообщение от клиента");
+
+        while (true)
+        {
+            byte[] buffer = udpClient.Receive(ref iPEndPoint);
+
+            if (buffer == null) break;
+            var messageText = Encoding.UTF8.GetString(buffer);
+
+            Message message = Message.DeserializeFromJson(messageText);
+            message.Print();
+
+            byte[] reply = Encoding.UTF8.GetBytes("Сообщение получено");
+            udpClient.Send(reply, reply.Length, iPEndPoint);
+        }
+    }
+
 }
---
+++ NetworkClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using Network;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //SentMessage(args[0], args[1]);
+            SentMessage("Mikhail", "127.0.0.1");
+        }
+
+        public static void SentMessage(string From, string ip)
+        {
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(ip), 12345);
+
+            while (true)
+            {
+                string messageText;
+                do
+                {
+                    Console.Write("Введите сообщение: ");
+                    messageText = Console.ReadLine();
+
+                }
+                while (string.IsNullOrEmpty(messageText));
+
+                Message message = new Message() { Text = messageText, NicknameFrom = From, NicknameTo = "Server", DateTime = DateTime.Now };
+                string json = message.SerializeMessageToJson();
+
+                byte[] data = Encoding.UTF8.GetBytes(json);
+                udpClient.Send(data, data.Length, iPEndPoint);
+
+                byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                if(buffer != null)
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer));
+                }
+            }
+
+        }
+    }
+}
